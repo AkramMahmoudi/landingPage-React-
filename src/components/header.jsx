a1572de5f6896854415673f4ac92387a551a3ba4
Akram Mahmoudi <akram.mahmoudi135@gmail.com>
2025-02-12 12:09:29 +0100

    header animation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,44 +1,106 @@
 // import React from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Pagination, EffectFade, Autoplay } from "swiper/modules";
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/effect-fade";
 
-// export const Header = (props) => {
+// export const Header = ({ data }) => {
 //   return (
 //     <header id="header">
-//       <div className="intro">
-//         <div className="overlay">
-//           <div className="container">
-//             <div className="row">
-//               <div className="col-md-8 col-md-offset-2 intro-text">
-//                 <h1>
-//                   {props.data ? props.data.title : "Loading"}
-//                   <span></span>
-//                 </h1>
-//                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-//                 <a
-//                   href="#features"
-//                   className="btn btn-custom btn-lg page-scroll"
-//                 >
-//                   Learn More
-//                 </a>{" "}
+//       <Swiper
+//         modules={[Pagination, EffectFade, Autoplay]}
+//         pagination={{ clickable: true }}
+//         effect="fade"
+//         autoplay={{ delay: 3000, disableOnInteraction: false }}
+//         className="header-slider"
+//       >
+//         {data?.images?.map((img, index) => (
+//           <SwiperSlide key={index}>
+//             <div
+//               className="intro"
+//               style={{
+//                 backgroundImage: `url(${img})`,
+//                 backgroundSize: "cover",
+//                 backgroundPosition: "center",
+//                 height: "100vh",
+//               }}
+//             >
+//               <div className="overlay">
+//                 <div className="container">
+//                   <div className="row">
+//                     <div className="col-md-8 col-md-offset-2 intro-text">
+//                       <h1>
+//                         {data?.title || "Loading"}
+//                         <span></span>
+//                       </h1>
+//                       <p>{data?.paragraph || "Loading"}</p>
+//                       <a
+//                         href="#features"
+//                         className="btn btn-custom btn-lg page-scroll"
+//                       >
+//                         Learn More
+//                       </a>
+//                     </div>
+//                   </div>
+//                 </div>
 //               </div>
 //             </div>
-//           </div>
-//         </div>
-//       </div>
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
 //     </header>
 //   );
 // };
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, EffectFade, Autoplay } from "swiper/modules";
+import { motion } from "framer-motion";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
-// let images = [
-//   "/img/etancheite-toit-terrasse-rouleaux-bitumineux.jpg",
-//   "/img/etancheite-toit-terrasse-rouleaux-bitumineux.jpg",
-//   "/img/etancheite-toit-terrasse-rouleaux-bitumineux.jpg",
-// ];
+
 export const Header = ({ data }) => {
+  const textVariants = {
+    hidden: { opacity: 0, y: 30 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+
+  const buttonVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 1.5 } },
+  };
+  const lineVariants = {
+    animate: {
+      y: ["-100%", "100%"],
+      transition: {
+        repeat: Infinity,
+        duration: 2, // Duration for each loop
+        ease: "linear", // Smooth animation
+      },
+    },
+  };
+  const lineVariants2 = {
+    animate: {
+      y: ["100%", "-100vh"],
+      transition: {
+        repeat: Infinity,
+        duration: 10, // Duration for each loop
+        ease: "linear", // Smooth animation
+      },
+    },
+  };
+  const lineVariants3 = {
+    animate: {
+      y: ["100%", "-100vh"],
+      transition: {
+        repeat: Infinity,
+        duration: 8, // Duration for each loop
+        ease: "linear", // Smooth animation
+      },
+    },
+  };
+
   return (
     <header id="header">
       <Swiper
@@ -48,7 +110,6 @@ export const Header = ({ data }) => {
         autoplay={{ delay: 3000, disableOnInteraction: false }}
         className="header-slider"
       >
-        {/* {console.log(data?.images)} */}
         {data?.images?.map((img, index) => (
           <SwiperSlide key={index}>
             <div
@@ -60,21 +121,105 @@ export const Header = ({ data }) => {
                 height: "100vh",
               }}
             >
+              {/* Animated Line */}
+              {/* left */}
+              <motion.div
+                className="animated-line"
+                variants={lineVariants}
+                animate="animate"
+                style={{
+                  position: "absolute",
+
+                  bottom: 0, // Start from the bottom
+                  left: "5%", // Center horizontally
+                  width: "1px", // Vertical line width
+                  height: "100%",
+                  backgroundColor: "#5ca9fb", // Line color
+                  zIndex: 2,
+                }}
+              />
+              <motion.div
+                className="animated-line"
+                variants={lineVariants}
+                animate="animate"
+                style={{
+                  position: "absolute",
+
+                  bottom: 0, // Start from the bottom
+                  left: "10%", // Center horizontally
+                  width: "1px", // Vertical line width
+                  height: "100%",
+                  backgroundColor: "#5ca9fb", // Line color
+                  zIndex: 2,
+                }}
+              />
+              {/* right */}
+              <motion.div
+                className="animated-line"
+                variants={lineVariants2}
+                animate="animate"
+                style={{
+                  position: "absolute",
+                  // top: "90%", // Center vertically
+                  // left: 0,
+                  // width: "100%",
+                  // height: "1px",
+                  bottom: 0, // Start from the bottom
+                  left: "95%", // Center horizontally
+                  width: "1px", // Vertical line width
+                  height: "30px",
+                  backgroundColor: "#5ca9fb", // Line color
+                  zIndex: 2,
+                }}
+              />
+              <motion.div
+                className="animated-line"
+                variants={lineVariants3}
+                animate="animate"
+                style={{
+                  position: "absolute",
+                  // top: "90%", // Center vertically
+                  // left: 0,
+                  // width: "100%",
+                  // height: "1px",
+                  bottom: 0, // Start from the bottom
+                  left: "90%", // Center horizontally
+                  width: "1px", // Vertical line width
+                  height: "30px",
+                  backgroundColor: "#5ca9fb", // Line color
+                  zIndex: 2,
+                }}
+              />
               <div className="overlay">
                 <div className="container">
                   <div className="row">
                     <div className="col-md-8 col-md-offset-2 intro-text">
-                      <h1>
+                      <motion.h1
+                        initial="hidden"
+                        animate="visible"
+                        variants={textVariants}
+                      >
                         {data?.title || "Loading"}
                         <span></span>
-                      </h1>
-                      <p>{data?.paragraph || "Loading"}</p>
-                      <a
+                      </motion.h1>
+                      <motion.p
+                        initial="hidden"
+                        animate="visible"
+                        variants={textVariants}
+                        transition={{ delay: 0.5 }}
+                      >
+                        {data?.paragraph || "Loading"}
+                      </motion.p>
+                      <motion.a
                         href="#features"
                         className="btn btn-custom btn-lg page-scroll"
+                        initial="hidden"
+                        animate="visible"
+                        variants={buttonVariants}
+                        transition={{ delay: 1 }}
                       >
                         Learn More
-                      </a>
+                      </motion.a>
                     </div>
                   </div>
                 </div>
